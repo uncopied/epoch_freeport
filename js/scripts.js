e7dc802945a56f5c98315d22f8d07c13b0d3b7b2
jesulonimi21 <anjola4jeez@gmail.com>
2021-06-16 11:48:14 +0100

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,126 @@
 console.log(algosdk);
+    let epochAsaInfo =[
+        {
+            name:"EPOCH, \"FREEPORT\", 2021; 1/5",
+            unit:"EPOCH",
+            url:"https://api.uncopied.art/c/v/C",
+            total:1,
+            metadataHash:btoa("73cd943cc7ef72d65d754662985412a4"),
+            decimals:0,
+            note: new Uint8Array(Buffer.from( "UNCOPIED_ASA=243599509"))
+        },
+        {
+            name:"EPOCH, \"FREEPORT\", 2021; 2/5",
+            unit:"EPOCH",
+            url:"https://api.uncopied.art/c/v/D",
+            total:1,
+            metadataHash:btoa("ddea2b97093aeff0f5c2b262eb562322"),
+            decimals:0,
+            note: new Uint8Array(Buffer.from( "UNCOPIED_ASA=243599550"))
+        },
+        {
+            name:"EPOCH, \"FREEPORT\", 2021; 3/5",
+            unit:"EPOCH",
+            url:"https://api.uncopied.art/c/v/E",
+            total:1,
+            metadataHash:btoa("aaede09d0799f5693e9dd627a0e22ec4"),
+            decimals:0,
+            note: new Uint8Array(Buffer.from( "UNCOPIED_ASA=243599602"))
+        },
+        {
+            name:"EPOCH, \"FREEPORT\", 2021; 4/5",
+            unit:"EPOCH",
+            url:"https://api.uncopied.art/c/v/F",
+            total:1,
+            metadataHash:btoa("bd2964d746eda8f42da3cdf21883bef3"),
+            decimals:0,
+            note: new Uint8Array(Buffer.from( "UNCOPIED_ASA=243599627"))
+        },
+        {
+            name:"EPOCH, \"FREEPORT\", 2021; 5/5",
+            unit:"EPOCH",
+            url:"https://api.uncopied.art/c/v/G",
+            total:1,
+            metadataHash:btoa("2cfc7e5bcb47fa235c1480c0b991fa76"),
+            decimals:0,
+            note: new Uint8Array(Buffer.from( "UNCOPIED_ASA=243599673"))
+        },
+        {
+            name:"EPOCH, \"FREEPORT\", 2021; 1/8",
+            unit:"EPOCH",
+            url:"https://api.uncopied.art/c/v/H",
+            total:1,
+            metadataHash:btoa("6099c8bd6d3e9db82a72cbd40c8a1771"),
+            decimals:0,
+            note: new Uint8Array(Buffer.from( "UNCOPIED_ASA=243599716"))
+
+        },
+        {
+            name:"EPOCH, \"FREEPORT\", 2021; 2/8",
+            unit:"EPOCH",
+            url:"https://api.uncopied.art/c/v/I",
+            total:1,
+            metadataHash:btoa("4bb069bf9c6bcc6b728217d9b2851926"),
+            decimals:0,
+            note: new Uint8Array(Buffer.from( "UNCOPIED_ASA=243599759"))
+
+        },
+        {
+            name:"EPOCH, \"FREEPORT\", 2021; 3/8",
+            unit:"EPOCH",
+            url:"https://api.uncopied.art/c/v/J",
+            total:1,
+            metadataHash:btoa("afabe9c45b609bcf4382f8e624174ebb"),
+            decimals:0,
+            note: new Uint8Array(Buffer.from( "UNCOPIED_ASA=243599805"))
+        },
+        {
+            name:"EPOCH, \"FREEPORT\", 2021; 4/8",
+            unit:"EPOCH",
+            url:"https://api.uncopied.art/c/v/K",
+            total:1,
+            metadataHash:btoa("d457a5ba58fb2e6cb09ab76dee991165"),
+            decimals:0,
+            note: new Uint8Array(Buffer.from( "UNCOPIED_ASA=243599860"))
+        },
+        {
+            name:"EPOCH, \"FREEPORT\", 2021; 5/8",
+            unit:"EPOCH",
+            url:"https://api.uncopied.art/c/v/L",
+            total:1,
+            metadataHash:btoa("0bff9605a2874529628bfa681a8dc586"),
+            decimals:0,
+            note: new Uint8Array(Buffer.from( "UNCOPIED_ASA=243599905"))
+        },
+        {
+            name:"EPOCH, \"FREEPORT\", 2021; 6/8",
+            unit:"EPOCH",
+            url:"https://api.uncopied.art/c/v/M",
+            total:1,
+            metadataHash:btoa("603dbf8fff0239be23d3984e0bb152d5"),
+            decimals:0,
+            note: new Uint8Array(Buffer.from( "UNCOPIED_ASA=243599951"))
+        },
+        {
+            name:"EPOCH, \"FREEPORT\", 2021; 7/8",
+            unit:"EPOCH",
+            url:"https://api.uncopied.art/c/v/N",
+            total:1,
+            metadataHash:btoa("b13f3cd280641cbe6055ab33341aef2e"),
+            decimals:0,
+            note: new Uint8Array(Buffer.from( "UNCOPIED_ASA=243599983"))
+        },
+        {
+            name:"EPOCH, \"FREEPORT\", 2021; 8/8",
+            unit:"EPOCH",
+            url:"https://api.uncopied.art/c/v/O",
+            total:1,
+            metadataHash:btoa("41bd0ef4a8589852c83f2ca5d2e39da1"),
+            decimals:0,
+            note: new Uint8Array(Buffer.from( "UNCOPIED_ASA=243600015"))
+        },
+
+    ]
  function createEpochNft(name,params,approvalProgram,clearProgram,price,from){
     let unitName = "EPOCH";
     let nftUrl = "epoch.gallery";
@@ -9,7 +131,7 @@ console.log(algosdk);
     let globalInts = 3;
     let localBytes =1;
     let localInts = 1;
-    let assetCreateTxn = createAsa(params,from,name,unitName,0,1,nftUrl,from,from,from,true);
+    let assetCreateTxn = createAsa(params,from,epochAsaInfo[0].name,epochAsaInfo[0].unit,0,epochAsaInfo[0].total,epochAsaInfo[0].url,from,from,from,true,epochAsaInfo[0].metadataHash,epochAsaInfo[0].note);
     let applicationCreateTxn = createApplicationTransaction(params,from,approvalProgram,clearProgram,localInts,localBytes,globalInts,globalBytes,appArgs);
     let otherAsaArray = [];
     for (let i = 0; i<=11;i++){
@@ -19,7 +141,7 @@ console.log(algosdk);
         }else{
             otherAsaName= `Epoch ${i+2}/8`
         }
-        otherAsaArray.push(createAsa(params,from,otherAsaName,name,0,1,nftUrl,from,from,from,true));
+        otherAsaArray.push( createAsa(params,from,epochAsaInfo[i+1].name,epochAsaInfo[i+1].unit,0,epochAsaInfo[i+1].total,epochAsaInfo[i+1].url,from,from,from,true,epochAsaInfo[i+1].metadataHash,epochAsaInfo[i+1].note));
     }
     let txns = [assetCreateTxn,applicationCreateTxn,...otherAsaArray];
     let groupId = algosdk.computeGroupID(txns);
@@ -27,6 +149,8 @@ console.log(algosdk);
         el.group=groupId;
         return el;
         });
+        console.log(otherAsaArray);
+        console.log(txns);
     return txns;
 }
 
@@ -84,7 +208,7 @@ function sendNftToArtist(){
 }
 
 
-function createAsa(params,from,assetName, assetUnitName,assetDecimals,assetTotal,assetUrl,assetFreeze,assetManager,assetReserve,assetDefaultFrozen){
+function createAsa(params,from,assetName, assetUnitName,assetDecimals,assetTotal,assetUrl,assetFreeze,assetManager,assetReserve,assetDefaultFrozen,assetMetadataHash,note){
    let  txn = {
       ...params,
       fee: 1000,
@@ -100,7 +224,9 @@ function createAsa(params,from,assetName, assetUnitName,assetDecimals,assetTotal
       assetManager: assetManager,
       assetReserve: assetReserve,
       assetDefaultFrozen: assetDefaultFrozen,
-      assetClawback:from
+      assetClawback:from,
+      assetMetadataHash,
+      note
     };
   return txn;
     // let signedTxn = await myAlgoWallet.signTransaction(txn);
